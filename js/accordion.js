169b92b93a8a2c87f91c40b0212962df7fce347a
Jose <palaualegriajosemanuel@gmail.com>
2020-01-28 23:54:30 +0100

    Acordeon funcional

--- js/accordion.js
+++ js/accordion.js
@@ -6,26 +6,35 @@ class Accordion {
         //Call to init and refresh accordion
         this.initAccordion();
     }
-    onClick = () => {
-            console.log(this);
+    onClick = (ev, i) => {
+        if (document.getElementById("panel__content" + i).style.display != "none") {
+            document.getElementById("panel__content" + i).style.display = "none";
+        } else {
+            document.getElementById("panel__content" + i).style.display = "initial";
+        }
+    }
+    createPanel = (panel, i) => {
+            let domPanel = newElement("div", "accordion__panel", "panel" + i);
+            domPanel.appendChild(newElement("h4", "panel__title", "", panel.title));
+            domPanel.appendChild(newElement("h5", "panel__subtitle", "", panel.subtitle));
+            //Button for collapse / uncollapse 
+            let button = newElement("div", "panel__arrow", "", "<i class='material-icons'>&#xE87C;</i>")
+            button.addEventListener('click', ev => this.onClick(ev, i));
+            domPanel.appendChild(button);
+            domPanel.appendChild(newElement("div", "panel__content", "panel__content" + i, panel.content, "display:none"));
+            return domPanel;
         }
         //This function inits and refresh the accordion
     initAccordion = () => {
-        //First we add the title to the Accordion
+        //First we clean the container
+        this.container.innerHTML = "";
+        //Then we add the title to the Accordion
         this.container.appendChild(newElement("h3", "accordion__title", "", this.mainTitle));
         //First iterates the panels
         for (let i = 0; i < this.panels.length; i++) {
             let panel = this.panels[i];
             //for each panel we write in the DOM a new DIV, with the title, the subtitle, the content and the action button. 
-            let domPanel = newElement("div", "accordion__panel", "panel" + i);
-            domPanel.appendChild(newElement("h4", "pannel__title", "", panel.title));
-            domPanel.appendChild(newElement("h5", "pannel__subtitle", "", panel.subtitle));
-            //Button for collapse / uncollapse 
-            let button = newElement("div", "pannel__arrow", "", "<i class='material-icons'>&#xE87C;</i>")
-            button.addEventListener('click', ev => this.onClick(ev));
-            domPanel.appendChild(button);
-            domPanel.appendChild(newElement("div", "pannel__content", "", panel.content));
-            this.container.appendChild(domPanel);
+            this.container.appendChild(this.createPanel(panel, i));
         }
     }
 }
--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,10 @@
-function newElement(tagName, className, id, innerHTML) {
+function newElement(tagName, className, id, innerHTML, style) {
     //this function creates a new element with the parameters.
     let ele = document.createElement(tagName);
     ele.id = id ? id : "";
     ele.className = className ? className : "";
     ele.innerHTML = innerHTML ? innerHTML : "";
+    ele.style = style ? style : ele.style;
+
     return ele;
 }
