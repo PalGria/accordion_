15833772ff62517bed4d33b1971769b6ccdb965f
Jose <palaualegriajosemanuel@gmail.com>
2020-01-29 20:04:08 +0100

    CSS first iteration

--- js/accordion.js
+++ js/accordion.js
@@ -1,16 +1,18 @@
 class Accordion {
     constructor(data) {
         this.container = document.getElementById(data.container);
+        this.container.classList += "custom__accordion"
+        this.id = data.container;
         this.mainTitle = data.mainTitle;
         this.panels = data.panels;
         //Call to init and refresh accordion
         this.initAccordion();
     }
     onClick = (ev, i) => {
-        if (document.getElementById("panel__content" + i).style.display != "none") {
-            document.getElementById("panel__content" + i).style.display = "none";
+        if (document.getElementById(this.id + "panel__content" + i).style.display != "none") {
+            document.getElementById(this.id + "panel__content" + i).style.display = "none";
         } else {
-            document.getElementById("panel__content" + i).style.display = "initial";
+            document.getElementById(this.id + "panel__content" + i).style.display = "initial";
         }
     }
     createPanel = (panel, i) => {
@@ -18,10 +20,10 @@ class Accordion {
             domPanel.appendChild(newElement("h4", "panel__title", "", panel.title));
             domPanel.appendChild(newElement("h5", "panel__subtitle", "", panel.subtitle));
             //Button for collapse / uncollapse 
-            let button = newElement("div", "panel__arrow", "", "<i class='material-icons'>&#xE87C;</i>")
+            let button = newElement("div", "panel__arrow", "", "<i class='material-icons'> keyboard_arrow_down </i>")
             button.addEventListener('click', ev => this.onClick(ev, i));
             domPanel.appendChild(button);
-            domPanel.appendChild(newElement("div", "panel__content", "panel__content" + i, panel.content, "display:none"));
+            domPanel.appendChild(newElement("div", this.id + "panel__content", this.id + "panel__content" + i, panel.content, "display:none"));
             return domPanel;
         }
         //This function inits and refresh the accordion
@@ -29,7 +31,7 @@ class Accordion {
         //First we clean the container
         this.container.innerHTML = "";
         //Then we add the title to the Accordion
-        this.container.appendChild(newElement("h3", "accordion__title", "", this.mainTitle));
+        this.container.appendChild(newElement("div", "accordion__title", "", this.mainTitle));
         //First iterates the panels
         for (let i = 0; i < this.panels.length; i++) {
             let panel = this.panels[i];
