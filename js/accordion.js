83eb03d3ca7d2e65748d52c7fe8cbd638e8a84e6
Jose <palaualegriajosemanuel@gmail.com>
2020-02-02 14:25:45 +0100

    Css tweaks

--- js/accordion.js
+++ js/accordion.js
@@ -21,8 +21,6 @@ class Accordion {
             target.classList.remove('collapsed');
             target.parentElement.classList.add('uncollapsed');
             arrow.innerHTML = "keyboard_arrow_up";
-
-
         }
     }
     createPanel = (panel, i) => {
@@ -46,40 +44,32 @@ class Accordion {
         this.container.innerHTML = "";
         //Then we add the title to the Accordion
         this.container.appendChild(newElement("div", "accordion__title", "", this.mainTitle));
-        this.container.appendChild(newElement("hr"));
         //First iterates the panels
         for (let i = 0; i < this.panels.length; i++) {
             let panel = this.panels[i];
             //for each panel we write in the DOM a new DIV, with the title, the subtitle, the content and the action button. 
             this.container.appendChild(this.createPanel(panel, i));
-            this.container.appendChild(newElement("hr"));
-
         }
     }
     collapse = (target) => {
         // get the height of the element's inner content, regardless of its actual size
-        var sectionHeight = element.scrollHeight;
-
+        let sectionHeight = element.scrollHeight;
         // temporarily disable all css transitions
-        var elementTransition = element.style.transition;
+        let elementTransition = element.style.transition;
         element.style.transition = '';
-
         // on the next frame (as soon as the previous style change has taken effect),
         // explicitly set the element's height to its current pixel height, so we 
         // aren't transitioning out of 'auto'
         requestAnimationFrame(function() {
             element.style.height = sectionHeight + 'px';
             element.style.transition = elementTransition;
-
             // on the next frame (as soon as the previous style change has taken effect),
             // have the element transition to height: 0
             requestAnimationFrame(function() {
                 element.style.height = 0 + 'px';
             });
         });
-
         // mark the section as "currently collapsed"
         element.setAttribute('data-collapsed', 'true');
-
     }
 }
